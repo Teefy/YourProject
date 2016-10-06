5c92ce068f7ea0369da48bcdf72ea4845dbfd6a5
Teefy <findir1@outlook.com>
2016-10-06 22:50:33 +0200

    initial checkin

---
+++ HelloWorld/src/HelloWorldTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		
+		HelloWorld hello = new HelloWorld();
+			 		
+		assertEquals("yes", hello.checkhome());
+		
+	}
+
+}
