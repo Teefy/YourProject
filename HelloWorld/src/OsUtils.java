26a8ed7b389a6fca226a02e6c4db360a59ce5439
Mvdweetering <Mvdweetering@Derp>
2016-10-14 14:23:45 +0100

    OS identify class toegevoegd

---
+++ HelloWorld/src/OsUtils.java
@@ -0,0 +1,20 @@
+
+public final class OsUtils {
+	
+   private static String OS = null;
+   public static String getOsName()
+   {
+      if(OS == null) 
+	      { 
+    	  OS = System.getProperty("os.name"); 
+    	  System.out.println(OS);
+	      }
+ 
+   return OS;
+   }
+
+   public static void main(String[] args) {
+	getOsName();
+	}
+
+}
