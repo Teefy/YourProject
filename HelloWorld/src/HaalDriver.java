de929865ca4d4c030247d50c2d045f803c96843d
youradministrator <youradministrator@youradministrator-FJ485AA-ABH-a6531-nl>
2016-10-19 13:39:42 +0200

    update HaalDriver

--- HelloWorld/src/HaalDriver.java
+++ HelloWorld/src/HaalDriver.java
@@ -7,15 +7,39 @@ public class HaalDriver {
 
 	public static  WebDriver MaakChrome()
 	{
-		System.setProperty("webdriver.chrome.driver", "c://windows//chromedriver.exe");	
-		WebDriver driver = new ChromeDriver();	
+		if (OsUtils.getOsName().equals("Windows 10")) {
+			System.setProperty("webdriver.chrome.driver", "c://windows//chromedriver.exe");	
+			System.out.println(OsUtils.getOsName());
+			}
+			
+	    if (OsUtils.getOsName().equals("Linux")) {
+	        System.setProperty("webdriver.chrome.driver", "/home/youradministrator/eclipse/chromedriver");
+	        System.out.println(OsUtils.getOsName());
+	        } 
+			
+		else {
+				
+			 }
+	        
+	    WebDriver driver = new ChromeDriver();	
  		return driver;
 	}
 	
-	public static  WebDriver MaakFirefox()
-	{
+	public static  WebDriver MaakFirefox() {
+		
+		if (OsUtils.getOsName().equals("Windows 10")) {
+			System.setProperty("webdriver.gecko.driver", "c://windows//geckodriver.exe");	
+			System.out.println(OsUtils.getOsName());
+			}
+		
+		if (OsUtils.getOsName().equals("Linux")) {
+	        System.setProperty("webdriver.gecko.driver", "/home/youradministrator/eclipse/geckodriver");
+	        System.out.println(OsUtils.getOsName());
+	        } 
+		
 		WebDriver driver = new FirefoxDriver();	
  		return driver;
+	
 	}
 	
 	public static  WebDriver MaakIE()
