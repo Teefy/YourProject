dbf34d9a1c67e0cfc8f9e10ab7f749c0dce7403d
Teefy <findir1@outlook.com>
2016-10-26 17:38:48 +0200

    Connect String Mysql

--- HelloWorld/src/LoginPortalTest.java
+++ HelloWorld/src/LoginPortalTest.java
@@ -10,20 +10,16 @@ public class LoginPortalTest {
 		mantisoperaties Tc1 = new mantisoperaties();
 		assertEquals("yes", Tc1.Nieuwebevinding("PRO", "testanalyst", "Chrome"));
 	}
-/*
+	
 	@Test
 	public void TC2() 
 	{
 		mantisoperaties Tc2 = new mantisoperaties();
 		assertEquals("yes", Tc2.Nieuwebevinding("PRO", "testanalyst", "Firefox"));
 	}
-<<<<<<< HEAD
-	*/
-=======
 	
 	@Test
 	public void TC3()
-	
 	{
 		mantisoperaties Tc3 = new mantisoperaties();
 		assertEquals ("yes", Tc3.ViewBevindingen("PRO", "testanalyst" , "Chrome"));
@@ -31,8 +27,4 @@ public class LoginPortalTest {
 		
 		
 	}
-	
-	
-	
->>>>>>> branch 'master' of https://github.com/Teefy/YourProject
 }
