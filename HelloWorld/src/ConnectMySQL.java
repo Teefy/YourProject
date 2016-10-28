dca8c0de15892ea8ac29797d812878f50e2b7592
youradministrator <youradministrator@localhost>
2016-10-28 13:52:57 +0200

    update sql

--- HelloWorld/src/ConnectMySQL.java
+++ HelloWorld/src/ConnectMySQL.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import com.mysql.jdbc.Statement;
 
 public class ConnectMySQL {
 
@@ -22,15 +21,13 @@ public class ConnectMySQL {
 		connection();
 		String resultaat = null;
 
-		String host = "jdbc:mysql://Derp:3306/yourdb";
+		String host = "jdbc:mysql://192.168.50.163:3306/yourdb";
 		String username = "Your";
 		String password = "Your";
 
 		try {
-			Connection connect = DriverManager.getConnection(host, username,
-					password);
-			PreparedStatement statement = (PreparedStatement) connect
-					.prepareStatement(SqlInput);
+			Connection connect = DriverManager.getConnection(host, username, password);
+			PreparedStatement statement = (PreparedStatement) connect .prepareStatement(SqlInput);
 			ResultSet data = statement.executeQuery();
 			data.next();
 
