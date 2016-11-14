db053bfd63d68e7ee3eaef7a9c3c6b39e88cb786
youradministrator <youradministrator@youradministrator>
2016-11-14 15:14:32 +0100

    TCs: createTestProject (in testlinkops(Test))

--- HelloWorld/src/codebase/TestlinkOps.java
+++ HelloWorld/src/codebase/TestlinkOps.java
@@ -2,7 +2,6 @@ package codebase;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -93,10 +92,13 @@ public String createTestProject (String Omgeving, String Rol, String Browser) {
 
 		if ((pageSource).contains(expectedContent))
 		{ 
-			System.out.println("TC GESLAAGD | Test Plan aangemaakt" );
+			System.out.println("✔ Test Project aangemaakt (en verwijderd)" );
 			returnvalue = "true"; 
+			// Verwijderen van aangemaakt issue tbv duplicaten
 			driver.findElement(By.xpath("//*[@id='item_view']/tbody/tr[2]/td[8]/img")).click();
 			driver.findElement(By.id("ext-gen20")).click();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
 			driver.close();
 			driver.quit();
 			
@@ -104,10 +106,119 @@ public String createTestProject (String Omgeving, String Rol, String Browser) {
 		
 		else
 		{ 
-			System.out.println("TC MISLUKT | Paginabron (bekijk nader):" + " " + pageSource );
+			System.out.println("✖ Geen Test Project aangemaakt");
 			returnvalue = "false"; 
+			driver.close();
+			driver.quit();
+			
+		}
+
+		return returnvalue;
+			
+}
+
+//__________________________________________________________________________________________________________
+
+
+public String createTestProjectFail (String Omgeving, String Rol, String Browser) {
+			
+		// Inloggen CP/T
+	
+		LoginPortal PortalInloggen = new LoginPortal();
+	
+		WebDriver driver = PortalInloggen.inloggen(Omgeving,Rol,Browser);
+
+		driver.manage().window().maximize(); 
+				
+		// Navigeren naar Testlink
+		
+		Testlinkobjecten.PijlRechts(driver).click();
+		
+		Testlinkobjecten.OpenTestLink(driver).click();
+		
+		// Navigeer naar Test Project Management
+		
+		driver.switchTo().frame("mainframe");
+		
+		Testlinkobjecten.OpenTestProjectManagement(driver).click();
+		
+		// Druk op Create-button
+		
+		Testlinkobjecten.CreateProjectButton(driver).click();
+		
+		// Invullen velden (later naar POM & SQL gooien) 
+
+		Testlinkobjecten.ProjectNaam(driver).sendKeys("");
+
+		// Prefix + Description (bereikt middels TAB) 
+		Testlinkobjecten.Prefix_en_Description(driver).sendKeys("TC" + Keys.TAB + 
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+		
+		
+		// Sectie: Enhanced features	
+		
+		Testlinkobjecten.InventoryRadiobutton(driver).click();
+
+		Testlinkobjecten.RequirementsRadiobutton(driver).click();
+		Testlinkobjecten.RequirementsRadiobutton(driver).click();
+		
+		Testlinkobjecten.PriorityRadiobutton(driver).click();
+		Testlinkobjecten.AutomationRadiobutton(driver).click();
+		
+		Testlinkobjecten.InventoryRadiobutton(driver).click();
+		
+		Testlinkobjecten.PriorityRadiobutton(driver).click();
+		Testlinkobjecten.AutomationRadiobutton(driver).click();
+
+		// Sectie: Issue Tracker Integration	
+		
+		Testlinkobjecten.IssueTrackerRadiobutton(driver).click();
+		Testlinkobjecten.IssueTrackerRadiobutton(driver).click();
+
+		new Select(Testlinkobjecten.IssueTrackerDropdown(driver)).selectByValue("1"); 
+		new Select(Testlinkobjecten.IssueTrackerDropdown(driver)).selectByValue("0"); 
+		
+		// Sectie: Availability
+		
+		Testlinkobjecten.ActiveRadiobutton(driver).click();
+		Testlinkobjecten.ActiveRadiobutton(driver).click();
+
+		Testlinkobjecten.PublicRadiobutton(driver).click();
+		Testlinkobjecten.PublicRadiobutton(driver).click();
+
+		// Sectie: Create of Cancel 
+		System.out.println(driver.findElement(By.id("error_icon_tprojectName")).getText()); 
+		
+		Testlinkobjecten.CreateProjectButton(driver).click();
+		//Testlinkobjecten.CancelProjectButton(driver).click();
+		
+		
+		// Verificatie van TC 
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		String pageSource = driver.getPageSource();
+		String expectedContent = "Automated Test Project";	
+		String error = driver.findElement(By.id("error_icon_tprojectName")).getText();
+		String returnvalue = null;
+
+		if ((pageSource).contains(expectedContent))
+		{ 
+			System.out.println("✖ Test Project aangemaakt (en verwijderd)" );
+			returnvalue = "true"; 
+			// Verwijderen van aangemaakt issue tbv duplicaten
 			driver.findElement(By.xpath("//*[@id='item_view']/tbody/tr[2]/td[8]/img")).click();
 			driver.findElement(By.id("ext-gen20")).click();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+			driver.close();
+			driver.quit();
+			
+		}	
+		
+		else if ((pageSource).contains(error))
+		{ 
+			System.out.println("✔ Geen Test Project aangemaakt (foutmelding getoont)");
+			returnvalue = "false"; 
 			driver.close();
 			driver.quit();
 			
@@ -118,3 +229,4 @@ public String createTestProject (String Omgeving, String Rol, String Browser) {
 }
 
 }
+
--- HelloWorld/src/testuitvoer/TestlinkOpsTest.java
+++ HelloWorld/src/testuitvoer/TestlinkOpsTest.java
@@ -7,19 +7,20 @@ import org.junit.Test;
 import codebase.TestlinkOps;
 
 public class TestlinkOpsTest {
-
+	
+	
 	@Test
-	public void TC01() {
+	public void createTestProject_TC01() {
 		
 		TestlinkOps TC01 = new TestlinkOps();
 		assertEquals ("true", TC01.createTestProject("PRO", "testleader", "Chrome"));
 	}
 
 	@Test
-	public void TC02() {
+	public void createTestProject_TC02() {
 		
 		TestlinkOps TC02 = new TestlinkOps();
-		assertEquals ( "true", TC02.createTestProject("PRO", "testleader", "Chrome"));
+		assertEquals ( "false", TC02.createTestProjectFail("PRO", "testleader", "Chrome"));
 	}
 
 }
